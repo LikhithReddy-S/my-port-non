bde8e45b49c2e661bfe20572de452a9f37ab1137
LikhithReddy-S <slikhithreddy22@gmail.com>
2025-06-13 20:59:41 +0530

    Done : Pop up Alert

---
+++ popup/popup.js
@@ -0,0 +1,7 @@
+function closePopup() {
+    document.getElementById('popupOverlay').style.display = 'none';
+}
+
+{/* // Optional: auto-close after a few seconds */}
+{/* // setTimeout(closePopup, 10000); // closes after 10 seconds */}
+
