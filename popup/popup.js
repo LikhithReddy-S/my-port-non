de4164c0b60e675988ef920165f1f1befbc0ba1f
LikhithReddy-S <slikhithreddy22@gmail.com>
2025-06-13 21:52:44 +0530

    fix :main portfolio loop with loalstorage

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,17 @@
-function closePopup() {
-    document.getElementById('popupOverlay').style.display = 'none';
-}
+// function closePopup() {
+//     document.getElementById('popupOverlay').style.display = 'none';
+// }
 
 {/* // Optional: auto-close after a few seconds */}
 {/* // setTimeout(closePopup, 10000); // closes after 10 seconds */}
 
+
+  // Show popup only if it hasn't already shown this session
+  const hasShownPopup = sessionStorage.getItem('maintenancePopupShown');
+
+  if (!hasShownPopup) {
+    document.getElementById('popup').style.display = 'flex';
+    sessionStorage.setItem('maintenancePopupShown', 'true');
+  } else {
+    document.getElementById('popup').style.display = 'none';
+  }
